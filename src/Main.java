9683a4324ce71aba70db4b2ee9607ba5e5b85541
kcheung3102 <kcheung3102@gmail.com>
2019-10-22 12:56:48 -0400

    + and -

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,24 @@ public class Main {
             System.out.println("Enter Exam Score");
             int examScore = grade.nextInt();
 
-            if (examScore >= 90 && examScore <= 100) {
-                System.out.println("A");
-            } else if (examScore >= 80 && examScore < 90) {
+            if (examScore >= 97 && examScore <= 100) {
+                System.out.println("A+");
+            } else if (examScore >= 94 && examScore < 97) {
+                    System.out.println("A");
+            } else if (examScore >= 90 && examScore < 94) {
+                System.out.println("A-");
+            } else if (examScore >= 87 && examScore < 90) {
+                System.out.println("B+");
+            } else if (examScore >= 84 && examScore < 87) {
                 System.out.println("B");
-            } else if (examScore >= 70 && examScore < 80) {
+            } else if (examScore >= 80 && examScore < 84) {
+                System.out.println("B-");
+            } else if (examScore >= 77 && examScore < 80) {
+                System.out.println("C+");
+            } else if (examScore >= 74 && examScore < 76) {
                 System.out.println("C");
+            } else if (examScore >= 70 && examScore < 74) {
+                System.out.println("C-");
             } else if (examScore >= 60 && examScore < 70) {
                 System.out.println("D");
             } else {
